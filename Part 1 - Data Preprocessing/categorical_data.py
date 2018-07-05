4a0453026aa3c18ad907a70d4cccf5a83c291ee4
vaibhav <vsrivastava@lwolf.com>
2018-07-04 21:58:43 -0500

    Data processing - categorical data

--- Part 1 - Data Preprocessing/categorical_data.py
+++ Part 1 - Data Preprocessing/categorical_data.py
@@ -1,28 +1,36 @@
 # Data Preprocessing
-
-# Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-
-# Taking care of missing data
+import matplotlib.pyplot as plot
+import pandas as panda
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# Encoding categorical data
-# Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-# Encoding the Dependent Variable
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+dataset = panda.read_csv('Data.csv')
+
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+imputer = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
+
+imputer = imputer.fit(X[:,1:3])
+
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+from sklearn.preprocessing import LabelEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+#array([[0, 44.0, 72000.0],
+#       [2, 27.0, 48000.0],
+#       [1, 30.0, 54000.0],
+#       [2, 38.0, 61000.0],
+#       [1, 40.0, nan],
+#       [0, 35.0, 58000.0],
+#       [2, nan, 52000.0],
+#       [0, 48.0, 79000.0],
+#       [1, 50.0, 83000.0],
+#       [0, 37.0, 67000.0]], dtype=object)
+
+from sklearn.preprocessing import OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
